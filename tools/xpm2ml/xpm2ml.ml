e0acb27638622cc772d428bfe7038373fcfdece4
Fabrice Le Fessant <fabrice@ocamlpro.com>
2013-11-01 13:08:24 +0100

    Fix generation of pixmaps
    Pixmaps where previously generated by Makefile. Now, they are
    also generated by ocp-build.

--- tools/xpm2ml/xpm2ml.ml
+++ tools/xpm2ml/xpm2ml.ml
@@ -4,7 +4,7 @@ let pixmap_name = Sys.argv.(2)
 let ocaml_name = Sys.argv.(3)
 
 let _ =
-  let oc = open_out "xpm2ml_c.c" in
+  let oc = Printf.kprintf open_out "%s_c.c" pixmap_name in
   Printf.fprintf oc "
 #include <stdio.h>
 #include \"%s\"
@@ -25,8 +25,12 @@ file_name
 pixmap_name
 pixmap_name;
   close_out oc;
-  assert (Sys.command "gcc -o xpm2ml_c xpm2ml_c.c" = 0);
-  assert (Sys.command (Printf.sprintf "./xpm2ml_c > %s" ocaml_name) = 0);
-  Sys.remove "xpm2ml_c.c";
-  Sys.remove "xpm2ml_c";
+  assert (
+    Printf.kprintf Sys.command
+      "gcc -o %s_c %s_c.c" pixmap_name pixmap_name = 0);
+  assert (
+     Printf.kprintf Sys.command
+       "./%s_c > %s" pixmap_name ocaml_name = 0);
+  Printf.kprintf Sys.remove "%s_c.c" pixmap_name;
+  Printf.kprintf Sys.remove "%s_c" pixmap_name;
   ()
