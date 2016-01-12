e737ffecaafb269c849a844e9060c5f27c778630
Fabrice Le Fessant <fabrice.le_fessant@ocamlpro.com>
2016-01-12 15:01:31 +0000

    [ocpwin] improve configure-wxMSW script `

--- config/config-wxMSW.ml
+++ config/config-wxMSW.ml
@@ -1,14 +1,112 @@
 
 (* Maybe add _DEBUG ? *)
-let ocamllib = Sys.argv.(1)
-let wxMSW_dir = Sys.argv.(2)
-let wxMSW_vc = Sys.argv.(3) (* vc100_x64 *)
+let wxMSW_dir = ref None
+let msvc = ref None
+let arch = ref (Some "x64")
+
+let arg_usage = 
+  String.concat "\n" [
+  "Usage is ./configure-wxMSW [OPTIONS]";
+  "Most options are mandatory the first time it is used.";
+  ]
+
+let () =
+  if Sys.file_exists "config.prev" then begin
+    let input_line ic = 
+      let s = input_line ic in
+      if s = "-" then None else Some s
+    in
+    let ic = open_in "config.prev" in
+    wxMSW_dir := input_line ic;
+    msvc := input_line ic;
+    arch := input_line ic;
+    close_in ic
+  end
+
+let arg_list = [
+  "-wx-dir", Arg.String
+    (fun s -> wxMSW_dir := Some s), "wxMSW_dir WX directory";
+  "-msvc", Arg.String
+    (fun s -> msvc := Some s), "MSVC Microsoft MSVC version (11.0 for example)";
+  "-arch", Arg.String
+    (fun s -> arch := Some s), "ARCH Architecture (either x64 or x86)";
+]
+
+let () =
+  Arg.parse arg_list 
+   (fun s -> 
+     Printf.eprintf "Error: unexpected argument %S\n%!" s;
+     Arg.usage arg_list arg_usage; exit 2) arg_usage
+
+let check_arg arg v = match !v with
+  None -> Printf.eprintf "Error: You must specify at least once %s\n%!" arg;
+          exit 2
+| Some s -> s
+
+let () =
+  let output_line oc s =
+    match !s with
+    | None -> output_string oc "-\n"
+    | Some s -> output_string oc s; output_string oc "\n"
+  in
+  let oc = open_out "config.prev" in
+  output_line oc wxMSW_dir;
+  output_line oc msvc;
+  output_line oc arch;
+  close_out oc
+
+let wxMSW_dir = check_arg "-wx-dir" wxMSW_dir
+let msvc = check_arg "-msvc" msvc
+let arch = check_arg "-arch" arch
+
+let wxMSW_vc =
+  match msvc with
+  | "9.0" -> "vc90"
+  | "10.0" -> "vc100"
+  | "11.0" -> "vc110"
+  | "12.0" -> "vc120"
+  | "14.0" -> "vc140"
+  | _ -> 
+     Printf.eprintf "Error: unsupported MSVC version %S\n%!" msvc;
+     Printf.eprintf "  Supported versions: 9.0, 10.0, 11.0, 12.0 and 14.0\n%!";
+     exit 2
+
+let wxMSW_vc =
+  match arch with
+  | "x64" -> wxMSW_vc ^ "_x64"
+  | "x86" -> wxMSW_vc
+  | _ ->
+     Printf.eprintf "Error: unsupported architecture %S\n%!" arch;
+     Printf.eprintf "  Supported architectures: x64, x86\n%!";
+     exit 2
 
 let () =
   Printf.printf "Configuring for:\n";
-  Printf.printf "OCaml directory: %S\n" ocamllib;
   Printf.printf "wxMSW directory = %S\n%!" wxMSW_dir;
-  Printf.printf "MSVC version = %S\n%!" wxMSW_vc
+  Printf.printf "wxMSW VC = %S\n%!" wxMSW_vc;
+  ()
+
+let () =
+  if Filename.is_relative wxMSW_dir || Filename.is_implicit wxMSW_dir then begin
+    Printf.eprintf "Error: wxMSW directory must be absolute\n%!";
+    exit 2
+  end
+
+let () =
+  let dlldir = Printf.sprintf "%s/lib/%s_dll" wxMSW_dir wxMSW_vc in
+  if not (Sys.file_exists dlldir) then begin
+    Printf.eprintf "Error: wxMSW directory does not contains libraries ?\n";
+    Printf.eprintf "   dlldir = %S\n%!" dlldir;
+    exit 2
+  end
+
+let () =
+  let incdir = Printf.sprintf "%s/include/msvc" wxMSW_dir in
+  if not (Sys.file_exists incdir) then begin
+    Printf.eprintf "Error: wxMSW directory does not contains includes ?\n";
+    Printf.eprintf "   incdir = %S\n%!" incdir;
+    exit 2
+  end
 
 let () =
   let oc = open_out "wxConfig/wxOCamlConfig.h" in
@@ -42,7 +140,7 @@ let () =
         [
           "-I"; Printf.sprintf "%s/include/msvc" wxMSW_dir;
           "-I"; Printf.sprintf "%s/include" wxMSW_dir;
-          "-I"; ocamllib;
+          "-I"; "%{OCAMLLIB}%";
           "-D_FILE_OFFSET_BITS=64";
           "-DWXUSINGDLL";
           "-D_UNICODE";
@@ -82,3 +180,14 @@ let () =
       ]
     );
   close_out oc
+  
+let () =
+  let cmd =  "ocp-build init" in
+  Printf.printf "Calling: %s\n" cmd;
+  if Sys.command cmd <> 0 then begin
+    Printf.eprintf "Error while calling %s\n%!" cmd;
+    exit 2;
+  end;
+  Printf.printf "\nYou should now call:\n";
+  Printf.printf "ocpwin -msvc %s -msvc-exec ocp-build\n" msvc;
+  ()
