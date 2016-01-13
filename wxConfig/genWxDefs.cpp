e37cf2ebd1f20421af6fdb1264a053ff8b5930b0
Fabrice Le Fessant <fabrice.le_fessant@ocamlpro.com>
2016-01-13 16:18:29 +0000

    Prevent generating unparsable constants on 32 bits
    Unfortunately, such constants are now rounded by removing its highest bits

--- wxConfig/genWxDefs.cpp
+++ wxConfig/genWxDefs.cpp
@@ -1,9 +1,9 @@
 #include "wxOCaml.h"
 #define exp2() 0
-#define CONSTINT(name, v) printf("let " #name " = %d\n",v);
+#define CONSTINT(name, v) printf("let " #name " = %d\n", (((value)(v)) << 1) >> 1);
 #define CONSTSELF(name) printf("let " #name " = %d\n",name);
 #define CSTBOOL(name) printf("let " #name " = %s\n", name ? "true" : "false" );
 int main(){
-#include "../api/wxDefs.dsc"
+  #include "../api/wxDefs.dsc"
   return 0;
 }
